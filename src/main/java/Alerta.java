d49cf16789d4fb00c0c6da704ddfcd8c917518c2
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-10-14 00:21:55 -0300

    Adicionando função que salva na tabela 'alerta'

---
+++ src/main/java/Alerta.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+
+public class Alerta {
+    private LocalDateTime dataAlerta;
+    private Integer registro;
+    private Integer fkComponente;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,8 @@ public class Main {
         JdbcTemplate template = databaseConfiguration.getTemplate();
 
         leImportaArquivoCsv("captura_total.csv");
+
+        salvaTabelaAlerta("captura_srv1.csv");
     }
 
     public static void leImportaArquivoCsv (String nomeArq) {
@@ -28,8 +30,6 @@ public class Main {
         Reader arq = null;
         BufferedReader entrada = null;
         BufferedWriter saida = null;
-
-        List<Captura> listaLida = new ArrayList<>();
 
         // Bloco try-catch para abrir o arquivo
         try {
@@ -139,6 +139,182 @@ public class Main {
 
         System.out.println("Processo finalizado");
 
+    }
+
+    public static void salvaTabelaAlerta(String nomeArq){
+        DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration();
+        JdbcTemplate template = databaseConfiguration.getTemplate();
+
+        System.out.println("Pegando informações dos limites do servidor 'srv1'");
+        String sqlSelectCpu = "select c.limite, t.nome from componentes c\n" +
+                "inner join servidores s on c.fkServidor = s.idServidor\n" +
+                "inner join tipoComponente t on t.idTipo = c.fkTipo\n" +
+                "where hostname = 'srv1' and  t.nome = 'Cpu'";
+
+        String sqlSelectRam = "select c.limite, t.nome from componentes c\n" +
+                "inner join servidores s on c.fkServidor = s.idServidor\n" +
+                "inner join tipoComponente t on t.idTipo = c.fkTipo\n" +
+                "where hostname = 'srv1' and t.nome = 'Memória'";
+
+        String sqlSelectDisco = "select c.limite, t.nome from componentes c\n" +
+                "inner join servidores s on c.fkServidor = s.idServidor\n" +
+                "inner join tipoComponente t on t.idTipo = c.fkTipo\n" +
+                "where hostname = 'srv1' and t.nome = 'Disco'";
+
+        List<ServidorComponente> capturaLimiteCpu = template.query(sqlSelectCpu, new BeanPropertyRowMapper<>(ServidorComponente.class));
+        List<ServidorComponente> capturaLimiteRam = template.query(sqlSelectRam, new BeanPropertyRowMapper<>(ServidorComponente.class));
+        List<ServidorComponente> capturaLimiteDisco = template.query(sqlSelectDisco, new BeanPropertyRowMapper<>(ServidorComponente.class));
+
+        Double limiteCpu = capturaLimiteCpu.get(0).getLimite();
+        Double limiteRam = capturaLimiteRam.get(0).getLimite();
+        Double limiteDisco = capturaLimiteDisco.get(0).getLimite();
+
+        Reader arq = null;
+        BufferedReader entrada = null;
+        BufferedWriter saida = null;
+        // Bloco try-catch para abrir o arquivo
+        try {
+            arq = new InputStreamReader(new FileInputStream(nomeArq), "UTF-8");
+            saida = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("saida.csv"), "UTF-8"));
+
+            entrada = new BufferedReader(arq);
+        } catch (IOException erro) {
+            System.out.println("Erro na abertura do arquivo");
+            System.exit(1);
+        }
+
+        System.out.println("Lendo o csv e escrevendo a saída");
+
+        try {
+            String[] registro;      // registro eh um vetor que armazenara cada parte da linha do arquivo
+            // readLine() eh usado   para ler uma linha inteira do arquivo
+            // Le a primeira linha do arquivo, que eh o cabecalho
+            String linha = entrada.readLine(); // linha eh a primeira linha do arquivo
+
+            // separa cada item da linha usando o delimitador ;
+            registro = linha.split(",");
+
+            String cabecalho = linha;
+
+            // Le a segunda linha do arquivo (1a linha de dados)
+            linha = entrada.readLine();
+            registro = linha.split(",");
+
+            Double primeiroRegistroCpu = Double.valueOf(registro[2]);
+            Double primeiroRegistroRam = Double.valueOf(registro[6]);
+            Double primeiroRegistroDisco = Double.valueOf(registro[12]);
+
+            Boolean podeRegistrarCpu = true;
+            Boolean podeRegistrarRam = true;
+            Boolean podeRegistrarDisco = true;
+
+
+            // verificando se o uso de cpu da primeira linha ultrapassa o limite
+            if(primeiroRegistroCpu > limiteCpu){
+                podeRegistrarCpu = false;
+                String dataHoraColeta = registro[1];
+
+                String sqlInsertAlertaCpu = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                        "(?, ?, ?)";
+                template.update(sqlInsertAlertaCpu, dataHoraColeta, primeiroRegistroCpu,  1);
+            }
+
+            // ram...
+            if(primeiroRegistroRam > limiteRam){
+                podeRegistrarRam = false;
+                String dataHoraColeta = registro[1];
+
+                String sqlInsertAlertaCpu = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                        "(?, ?, ?)";
+                template.update(sqlInsertAlertaCpu, dataHoraColeta, primeiroRegistroRam,  2);
+            }
+
+            // disco...
+            if(primeiroRegistroDisco > limiteDisco){
+                podeRegistrarDisco = false;
+                String dataHoraColeta = registro[1];
+
+                String sqlInsertAlertaCpu = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                        "(?, ?, ?)";
+                template.update(sqlInsertAlertaCpu, dataHoraColeta, primeiroRegistroDisco,  3);
+            }
+
+            while (linha != null) { // enquanto nao chegou ao final do arquivo
+                registro = linha.split(",");
+                // converte de String para Integer usando Integer.valueOf
+                // Se fosse converter de String para int usa-se Integer.parseInt
+                String nomeDaMaquina = registro[0];
+                String dataHoraColeta = registro[1];
+                Double usoCPU = Double.valueOf(registro[2]);
+                Double usoRAM = Double.valueOf(registro[6]);
+                Double usoDisco = Double.valueOf(registro[12]);
+
+                if(usoCPU < limiteCpu){
+                    podeRegistrarCpu = true;
+                }
+
+                if(usoRAM < limiteRam){
+                    podeRegistrarRam = true;
+                }
+
+                if(usoDisco < limiteDisco){
+                    podeRegistrarDisco = true;
+                }
+
+                for (ServidorComponente sc : capturaLimiteCpu) {
+                    String tipoComponente = sc.getNome();
+                    Double limite = sc.getLimite();
+
+                    if(tipoComponente.equalsIgnoreCase("cpu") && usoCPU > limite && podeRegistrarCpu){
+                        String sqlInsertAlertaCpu = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                                "(?, ?, ?)";
+                        template.update(sqlInsertAlertaCpu, dataHoraColeta, usoCPU, 1);
+                        podeRegistrarCpu = false;
+                    }
+
+                }
+
+                for (ServidorComponente sc : capturaLimiteRam) {
+                    String tipoComponente = sc.getNome();
+                    Double limite = sc.getLimite();
+
+                    if(tipoComponente.equalsIgnoreCase("memória") && usoRAM > limite && podeRegistrarRam){
+                        String sqlInsertAlertaRam = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                                "(?, ?, ?)";
+                        template.update(sqlInsertAlertaRam, dataHoraColeta, usoRAM, 2);
+                        podeRegistrarRam = false;
+                    }
+
+                }
+
+                for (ServidorComponente sc : capturaLimiteDisco) {
+                    String tipoComponente = sc.getNome();
+                    Double limite = sc.getLimite();
+
+                    if(tipoComponente.equalsIgnoreCase("disco") && usoDisco > limite && podeRegistrarDisco){
+                        String sqlInsertAlertaDisco = "insert into alerta (data_alerta, registro, fkComponente) values" +
+                                "(?, ?, ?)";
+                        template.update(sqlInsertAlertaDisco, dataHoraColeta, usoDisco, 3);
+                        podeRegistrarDisco = false;
+                    }
+
+                }
+                // Le a proxima linha do arquivo
+                linha = entrada.readLine();
+            } // final do while
+        } // final do try
+        catch (IOException erro) {
+            System.out.println("Erro ao ler o arquivo");
+            erro.printStackTrace();
+        } finally {
+            try {
+                entrada.close();
+                arq.close();
+            } catch (IOException erro) {
+                System.out.println("Erro ao fechar o arquivo");
+            }
+        }
     }
 
 }
